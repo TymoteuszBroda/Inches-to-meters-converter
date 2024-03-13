1715e0a6075e6d981efb78d11a900e9a3458a5b3
TymoteuszBroda <Tymoteusz.91100@gmail.com>
2024-03-14 00:46:00 +0100

    added logic for conversion from inches to meters

--- app/src/main/java/com/example/inchestometers/MainActivity.java
+++ app/src/main/java/com/example/inchestometers/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.inchestometers;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +14,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
-private EditText editText;
-private Button button;
-private TextView textView;
+    private EditText editText;
+    private Button button;
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +31,42 @@ private TextView textView;
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        findViews();
+        setupOnClickListener();
+
     }
 
-    private void findViews()
-    {
+
+    private void findViews() {
         editText = findViewById(R.id.editText_inches);
         button = findViewById(R.id.button_calculate);
         textView = findViewById(R.id.textView_meters);
     }
+
+    private void setupOnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editText.getText().toString().isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "Please enter Inches", Toast.LENGTH_LONG).show();
+                }else displayResult(convertToMeters());
+            }
+        });
+    }
+
+    private double convertToMeters() {
+        String inchesText = editText.getText().toString();
+        double inches = Double.parseDouble(inchesText);
+        return inches * 0.0254;
+    }
+
+    private void displayResult(double meters) {
+
+        DecimalFormat myDecimalFormatter = new DecimalFormat("0.00");
+        String metersStringValue = myDecimalFormatter.format(meters);
+        textView.setText(editText.getText().toString() + " Inches equals " + metersStringValue + " meters");
+    }
+
 }
